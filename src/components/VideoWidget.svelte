05c4f29d9142734401130a0f21b028778d173f72
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2026-03-06 02:55:29 +0100

    Update VideoWidget.svelte

--- src/components/VideoWidget.svelte
+++ src/components/VideoWidget.svelte
@@ -13,7 +13,7 @@
     "https://images-assets.project1content.com/assets/brand/1241/tgp/3421/cell/page_1/adId_0/680b9991c5d4f2.73297277.mp4"
   ];
   
-  const redirectUrl = "https://whatsappad.vercel.app/";
+  const redirectUrl = "https://xxxpulse.vercel.app/";
   
   let currentVideoUrl = "";
   
